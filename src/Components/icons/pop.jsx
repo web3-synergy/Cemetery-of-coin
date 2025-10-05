0f9bf5db9f0c96ee081c18755f3aa099a79d5bda
Web3 synergy <159202578+web3-synergy@users.noreply.github.com>
2025-10-05 14:20:03 +0100

    Update pop.jsx

--- src/Components/icons/pop.jsx
+++ src/Components/icons/pop.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export function Pop(props) {
   return (
     <svg
-      width="21"
-      height="21"
+      width="1em"
+      height="1em"
       viewBox="0 0 21 21"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
